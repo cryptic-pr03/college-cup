433de79f49a8bae91d22471085eff8a96842bafe
Priyanshu Raman <92126782+cryptic-pr03@users.noreply.github.com>
2022-08-09 16:14:29 +0530

    routing-added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import NotesPage from "./pages/NotesPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import Projects from "./pages/ProjectsPage";
 
 function App() {
-  return <div className="App">APP COMPONENT</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="notes" element={<NotesPage />} />
+        <Route path="projects" element={<ProjectsPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

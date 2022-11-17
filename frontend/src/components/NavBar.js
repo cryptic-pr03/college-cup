50c640246628a9c77328a1e446e24b03ece5173a
james3gh <jamesthomas3801@gmail.com>
2022-11-18 01:15:55 +0530

    scs logo and text added

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -8,7 +8,21 @@ function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="/">College-Cup</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/Scs-Logo.png"
+            width="50"
+            height="50"
+            className="d-inline-block align-center"
+          />{" "}
+          <span
+            className="badge text-wrap align-center"
+            style={{ fontSize: "18px", width: "9rem" }}
+          >
+            College-Cup by SCS
+          </span>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
